bf4422e82b712f84e07af7c51312c77073cdb57e
Dmitry Sergeev <dmitrijsergeev@gmail.com>
2024-08-06 18:09:08 +0400

    table

--- src/components/auth/login-form/login-form.stories.tsx
+++ src/components/auth/login-form/login-form.stories.tsx
@@ -12,5 +12,9 @@ export default meta
 type Story = StoryObj<typeof meta>
 
 export const Default: Story = {
-  args: {},
+  args: {
+    password: '456',
+    rememberMe: false,
+    userName: 'Dmitry',
+  },
 }
--- src/components/auth/login-form/login-form.tsx
+++ src/components/auth/login-form/login-form.tsx
@@ -2,8 +2,15 @@ import { useForm } from 'react-hook-form'
 
 import s from './login-form.module.scss'
 
-export const LoginForm = () => {
-  const { handleSubmit, register } = useForm()
+type LoginFormProps = {
+  password?: string
+  rememberMe?: boolean
+  userName?: string
+}
+
+export const LoginForm = (props: LoginFormProps) => {
+  const {} = props
+  const { handleSubmit, register } = useForm<LoginFormProps>()
 
   const onSubmit = handleSubmit(data => {
     console.log(data)
@@ -18,13 +25,24 @@ export const LoginForm = () => {
           <label className={s.label} htmlFor={'user'}>
             username
           </label>
-          <input className={s.input} id={'user'} {...register('user')} />
+          <input className={s.input} id={'user'} {...register('userName')} type={'email'} />
         </div>
         <div>
           <label className={s.label} htmlFor={'password'}>
             password
           </label>
-          <input className={s.input} id={'password'} {...register('password')} />
+          <input className={s.input} id={'password'} {...register('password')} type={'password'} />
+        </div>
+        <div>
+          <label className={s.label} htmlFor={'rememberMe'}>
+            rememberMe
+          </label>
+          <input
+            className={s.input}
+            id={'rememberMe'}
+            {...register('rememberMe')}
+            type={'checkbox'}
+          />
         </div>
         <button className={s.button} type={'submit'}>
           send
